15c5a7fa0d7c338c0f64e272118ca76b9857b6a8
Antonin Stefanutti <antonin@stefanutti.fr>
2016-08-23 19:12:24 +0200

    Add books API test

--- src/test/java/io/fabric8/quickstarts/camel/ApplicationTest.java
+++ src/test/java/io/fabric8/quickstarts/camel/ApplicationTest.java
@@ -24,10 +24,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -63,5 +66,20 @@ public class ApplicationTest {
         assertThat(order.getBook().getItem()).isIn("Camel", "ActiveMQ");
         assertThat(order.getBook().getDescription()).isIn("Camel in Action", "ActiveMQ in Action");
         assertThat(order.isProcessed()).isTrue();
+    }
+
+    @Test
+    public void booksTest() {
+        ResponseEntity<List<Book>> response = restTemplate.exchange("/camel-rest-sql/books",
+            HttpMethod.GET, null, new ParameterizedTypeReference<List<Book>>(){});
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        List<Book> books = response.getBody();
+        assertThat(books).hasSize(2);
+        assertThat(books).element(0)
+            .hasFieldOrPropertyWithValue("item", "Camel")
+            .hasFieldOrPropertyWithValue("description", "Camel in Action");
+        assertThat(books).element(1)
+            .hasFieldOrPropertyWithValue("item", "ActiveMQ")
+            .hasFieldOrPropertyWithValue("description", "ActiveMQ in Action");
     }
 }
