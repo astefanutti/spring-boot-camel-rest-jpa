61e876248b239aaf88c2b760a14bd60e49949fd2
Antonin Stefanutti <antonin@stefanutti.fr>
2016-11-17 16:29:33 +0100

    Call the REST API from the Kubernetes test

--- src/test/java/io/fabric8/quickstarts/camel/KubernetesIntegrationKT.java
+++ src/test/java/io/fabric8/quickstarts/camel/KubernetesIntegrationKT.java
@@ -18,27 +18,56 @@ package io.fabric8.quickstarts.camel;
 import io.fabric8.annotations.ServiceName;
 import io.fabric8.kubernetes.api.model.Service;
 import io.fabric8.kubernetes.client.KubernetesClient;
+import org.assertj.core.api.Assertions;
 import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.junit.InSequence;
 import org.jboss.arquillian.test.api.ArquillianResource;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
+import java.util.List;
+
+import static io.fabric8.kubernetes.api.KubernetesHelper.getServiceURL;
 import static io.fabric8.kubernetes.assertions.Assertions.assertThat;
 
 @RunWith(Arquillian.class)
 public class KubernetesIntegrationKT {
 
     @ArquillianResource
-    KubernetesClient client;
+    private KubernetesClient client;
 
     @ArquillianResource
     @ServiceName("camel-rest-jpa")
-    Service service;
+    private Service service;
 
     @Test
     @RunAsClient
+    @InSequence(1)
     public void testAppProvisionsRunningPods() {
         assertThat(client).deployments().pods().isPodReadyForPeriod();
+    }
+
+    @Test
+    @RunAsClient
+    @InSequence(2)
+    public void booksTest() {
+        TestRestTemplate restTemplate = new TestRestTemplate();
+        ResponseEntity<List<Book>> response = restTemplate.exchange(getServiceURL(service) + "/camel-rest-jpa/books",
+            HttpMethod.GET, null, new ParameterizedTypeReference<List<Book>>(){});
+        Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        List<Book> books = response.getBody();
+        Assertions.assertThat(books).hasSize(2);
+        Assertions.assertThat(books).element(0)
+            .hasFieldOrPropertyWithValue("item", "Camel")
+            .hasFieldOrPropertyWithValue("description", "Camel in Action");
+        Assertions.assertThat(books).element(1)
+            .hasFieldOrPropertyWithValue("item", "ActiveMQ")
+            .hasFieldOrPropertyWithValue("description", "ActiveMQ in Action");
     }
 }
